2db1f1d90eb46dd8766adc5a438e10f9c3666fd8
Thomas Koscheck <thomas.koscheck@protonmail.ch>
2016-06-12 00:10:15 +0200

    Kommentare hinzugefügt

--- Datendownload/csv_parser.py
+++ Datendownload/csv_parser.py
@@ -1,3 +1,6 @@
+## Pythonscript für den Raspberry Pi um aktuelle Feinstaubdaten für Ulm herunterzuladen.
+## Außerdem wird aus der Datei der entsprechende String ausgelesen und in entsprechendes Format konvertiert
+
 #!/usr/bin/python3.4
 import csv
 import urllib.request
@@ -5,21 +8,16 @@ import RPi.GPIO as GPIO
 import time
 import os
 
-#Folgendes nimmt das Aktuelle Datum und konvertiert es in ein fuer die url
-#funktionierendes Format
-
+## Holt sich das aktuelle Systemdatum und konvertiert es, damit es in einem fuer die URL verarbeitbaren Format ist
 datetime = time.localtime()
 heute = time.strftime("%Y%m%d", datetime)
 gestern = str(int(heute)-1)
 
-#print(heute+ " " + gestern) #test
-
 os.system("rm -f ~/feinstaub_werte")
 urllib.request.urlretrieve("http://www.umweltbundesamt.de/luftdaten/data.csv?pollutant=PM1&data_type=1TMW&date=%s&dateTo=%s&station=DEBW019" % (gestern,heute), "/home/pi/feinstaub_werte" )
 
-#Folgender Code ist nich sehr schoen. Er soll die letzte Zahl der Datei
-#feinstaub_werte extrahieren
-
+## Folgender Code ist nicht sehr schoen.
+## Extrahiert die letzte Zahl aus der Datei feinstaub_werte 
 feinstaub_werte = open("feinstaub_werte").read()
 feinstaub_werte = feinstaub_werte.split()
 feinstaub_werte = feinstaub_werte[len(feinstaub_werte)-1]
@@ -27,19 +25,21 @@ feinstaub_werte = feinstaub_werte.split(sep = ";")
 feinstaub_werte = feinstaub_werte[len(feinstaub_werte)-1]
 feinstaub_werte = int(feinstaub_werte)
 
-print(feinstaub_werte) #test
+print(feinstaub_werte) 
 
-#Schreibt pins an
+## schreibt Pins an
 grueneLuft = 20
 gelbeLuft = 30
 orangeneLuft = 40
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(0, GPIO.OUT) #gruen
-GPIO.setup(2, GPIO.OUT) #gelb
-GPIO.setup(3, GPIO.OUT) #orange
-GPIO.setup(7, GPIO.OUT) #rot
+GPIO.setup(0, GPIO.OUT) #gruener Smiley
+GPIO.setup(2, GPIO.OUT) #gelber Smiley
+GPIO.setup(3, GPIO.OUT) #orangener Smiley
+GPIO.setup(7, GPIO.OUT) #roter Smiley
 
+
+## entsprechend der extrahierten Werte werden die Pins auf HIGH bzw. LOW gezogen
 if feinstaub_werte <= grueneLuft:
         GPIO.output(0, GPIO.HIGH)
         GPIO.output(2, GPIO.LOW)
