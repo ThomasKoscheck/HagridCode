b3fc10df4ec13c7e7c3f26ad7b37ab98e624d1cf
Thomas Koscheck <thomas.koscheck@protonmail.ch>
2016-06-11 12:41:15 +0200

    wifi connection
    wifi connection enabled

--- Datendownload/Datendownload.ino
+++ Datendownload/Datendownload.ino
@@ -17,51 +17,73 @@
 //Downloadlink zum Wert von Ulm am 10.06 bis 11.06
 //http://www.umweltbundesamt.de/luftdaten/data.csv?pollutant=PM1&data_type=1TMW&date=20160610&dateTo=20160611&station=DEBW019
  
-#include <ESP8266WiFi.h>
-
-#define TOKEN "YOUR THING TOKEN"
-
 // WiFi information
-#define SSID      "jugend_hackt"
-#define PASS_SSID "aegheex9ieTheine"
+const char* ssid     = "jugend_hackt";
+const char* password = "aegheex9ieTheine";
 
-WiFiClient client;
+const char* host = "google.com";
 
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void checkConnection() {
-    if (WiFi.status() != WL_CONNECTED) {
-        WiFi.begin(SSID, PASS_SSID);
-        while (WiFi.status() != WL_CONNECTED) {
-            Serial.print(".");
-            delay(1000);
-        }
-    }
-    if (!client.connected()) {
-        if (client.connect("api.thethings.io", 80)) {
-            //client.println("GET /v2/things/" + String(TOKEN) + " HTTP/1.1");
-            client.println("Host: api.thethings.io");
-            client.println("Accept: application/json");
-            client.println();
-        }
-    }
-}
+int value = 0;
 
 void loop() {
-    checkConnection();
-    String string;
-    while (client.available()) {
-        string.concat((char)client.read());
-    }
-    string.toUpperCase();
-    string.replace(" ", "");
-    if (string.indexOf("\"VALUE\":1") > 0) {
-        Serial.println("ON");
-    }
-    else if (string.indexOf("\"VALUE\":0") > 0) {
-        Serial.println("OFF");
-    }
+  delay(5000);
+  ++value;
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  // We now create a URI for the request
+  String url = "/stan";
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(10);
+
+  // Read all the lines of the reply from server and print them to Serial
+  Serial.println("Respond:");
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
 }
 
